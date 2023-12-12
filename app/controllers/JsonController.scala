361d1ce0fdd98d0be3fb1db68a34f2c38459fa91
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2023-12-12 23:05:16 +0000

    handling json

--- app/controllers/JsonController.scala
+++ app/controllers/JsonController.scala
@@ -10,8 +10,9 @@ import scala.concurrent.Future
 @Singleton class JsonController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with Logging:
 
   def returnHello(name: String): Action[AnyContent] = Action.async { implicit request: Request[AnyContent] =>
-    val json = Json.toJson(Hello(name))
-    Future.successful(Ok(json))
+    val greeting = Hello(name)
+    val jsonGreeting = Json.toJson(greeting)
+    Future.successful(Ok(jsonGreeting))
   }
 
 case class Hello(name: String)
